fb65ecb6a7afd153503c262698e8b77ed4b6a894
871234342 <a871234342.cs07@nctu.edu.tw>
2021-12-12 17:41:44 +0800

    UCB structure change

--- mcts.h
+++ mcts.h
@@ -41,42 +41,49 @@ public:
     }
 
     int get_visit_count() {
-        return played;
+        return N;
     }
 
     action::place get_move() {
         return parent_move;
     }
 
-    double UBC(int parent_played) {
-        double log_parent_played = std::log(parent_played);
-        double explore = std::sqrt(log_parent_played / played);
-        double exploit = (double)win / (double)played;
-        // std::cout<<exploit<<"+"<<explore<<"  \n";
-        // if (exploit > 1) {
-        //     char t;
-        //     std::cout<<"HMM?: "<<win<<"/"<<played<<'\n';
-        //     std::cin>>t;
-        // }
-        return explore + 0.7 * exploit;
+    double UCB(int parent_played = 0) {
+        return Q + 0.2 * std::sqrt(std::log(parent->N) / N);
+    }
+
+    double UCB_tuned() {
+        // count V(n)
     }
 
     /**
      * return the child node with highest UCB, or itself if the node has unexplored child
      */
-    node* select() {
+    node* select(bool tuned = false) {
         //std::cout<<"Selection started!      ";
         if (num_of_child != explored_child || num_of_child == 0)     return this;
         double best_value = 0;
         node* best=NULL;
         //std::cout<<"Iterating...\n";
-        for (node* child : children) {
-            double value = child->UBC(played);
-            if (value >= best_value) {
-                best_value = value;
-                best = child;
-            }
+        if (tuned){
+            for (node* child : children) {
+                double value = child->UCB();
+                if (value >= best_value) {
+                    best_value = value;
+                    best = child;
+                }
+            }              
         }
+        else {
+            for (node* child : children) {
+                double value = child->UCB();
+                if (value >= best_value) {
+                    best_value = value;
+                    best = child;
+                }
+            }            
+        }
+
         //std::cout<<"  done with best_score="<<best_value<<'\n';
         return best;
     }
@@ -157,8 +164,8 @@ public:
      * return the parent node to update
      */
     node* update(bool victory) {
-        played++;
-        if (victory)    win++;
+        N++;
+        Q += (victory - Q) / N;
         return parent;
     }
 
@@ -177,13 +184,15 @@ public:
 
 private:
     board current;
-    int num_of_child = 0, explored_child = 0, win = 0, played = 0;
+    int num_of_child = 0, explored_child = 0;
     board::piece_type who;  //type to play next
     board::piece_type child_type;
     node* parent = NULL;
     action::place parent_move;
     std::vector<node*> children;
     bool terminated = false;
+    int N = 0;
+    double Q = 0;
 };
 
 class mcts{
